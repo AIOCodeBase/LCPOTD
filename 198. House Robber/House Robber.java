8b88b2dabb1a35747dc87b9a4796f5deb0d74cff
Soumyadeep Mandal <imsamprodeveloper@gmail.com>
2022-12-14 15:21:41 +0530

    House Robber

---
+++ 198. House Robber/House Robber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int rob(int[] num) {
+        int[][] dp = new int[num.length + 1][2];
+        for (int i = 1; i <= num.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = num[i - 1] + dp[i - 1][0];
+        }
+        return Math.max(dp[num.length][0], dp[num.length][1]);
+    }
+}
