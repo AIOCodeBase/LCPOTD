9db19340702d2a3af63403ec376927a6dcb35dcc
Soumyadeep Mandal <imsamprodeveloper@gmail.com>
2022-12-25 13:44:07 +0530

    Longest Subsequence With Limited Sum

---
+++ 2389. Longest Subsequence With Limited Sum/Approach 2 - Prefix Sum + Binary Search/Longest Subsequence With Limited Sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; ++i)
+            nums[i] += nums[i - 1];
+        
+        int n = queries.length;
+        int answer[] = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int index = binarySearch(nums, queries[i]);
+            answer[i] = index;
+        }
+        
+        return answer;
+    }
+    
+    int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid + 1;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return nums[left] > target ? left : left + 1;
+    }
+}
