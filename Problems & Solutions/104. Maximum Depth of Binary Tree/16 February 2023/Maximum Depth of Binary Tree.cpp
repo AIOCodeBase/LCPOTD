c55f273896e92c1748e052514686e4b3f96aad10
Soumyadeep Mandal <imsamprodeveloper@gmail.com>
2023-02-16 20:58:37 +0530

    104. Maximum Depth of Binary Tree - 16 February 2023

---
+++ Problems & Solutions/104. Maximum Depth of Binary Tree/16 February 2023/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+};
