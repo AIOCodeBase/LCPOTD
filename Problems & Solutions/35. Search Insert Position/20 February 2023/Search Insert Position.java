392d5bc98a69021141773391c1bcf5bb60378bda
Soumyadeep Mandal <imsamprodeveloper@gmail.com>
2023-02-20 13:25:32 +0530

    35. Search Insert Position - 20 February 2023
    Closes #310 & #311

---
+++ Problems & Solutions/35. Search Insert Position/20 February 2023/Search Insert Position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] A, int target) {
+    int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}
